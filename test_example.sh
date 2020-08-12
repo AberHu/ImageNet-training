1002a545ef2c804b3f9b19979aaf74ec4d14fe43
Aber Hu <huyibo871079699@163..com>
2020-08-12 19:48:35 +0800

    add example scripts

--- test_example.sh
+++ test_example.sh
@@ -1,7 +1,7 @@
 CUDA_VISIBLE_DEVICES=0 python -u test.py \
-				--val_root "Your ImageNet Val Set Path" \
-				--val_list "ImageNet Val List" \
-				--weights "Pretrained Weights" \
-				--model 'MobileNetV3_Large' \
-				--trans_mode 'tv' \
-				--ema
+	--val_root "Your ImageNet Val Set Path" \
+	--val_list "ImageNet Val List" \
+	--weights "Pretrained Weights" \
+	--model 'MobileNetV3_Large' \
+	--trans_mode 'tv' \
+	--ema
--- train_example.sh
+++ train_example.sh
@@ -1,27 +1,27 @@
 CUDA_VISIBLE_DEVICES=0,1 python -u -m torch.distributed.launch --nproc_per_node=2 train.py \
-				--train_root "Your ImageNet Train Set Path" \
-				--val_root "Your ImageNet Val Set Path" \
-				--train_list "ImageNet Train List" \
-				--val_list "ImageNet Val List" \
-				--save './checkpoints/' \
-				--workers 16 \
-				--epochs 250 \
-				--warmup_epochs 5 \
-				--batch_size 512 \
-				--lr 0.2 \
-				--lr_min 0.0 \
-				--lr_scheduler 'cosine_epoch' \
-				--momentum 0.9 \
-				--weight_decay 3e-5 \
-				--no_wd_bias_bn \
-				--model 'MobileNetV3_Large' \
-				--num_classes 1000 \
-				--dropout_rate 0.2 \
-				--label_smooth 0.1 \
-				--trans_mode 'tv' \
-				--color_jitter \
-				--ema_decay 0.9999 \
-				--opt_level 'O1' \
-				--note 'try'
+	--train_root "Your ImageNet Train Set Path" \
+	--val_root "Your ImageNet Val Set Path" \
+	--train_list "ImageNet Train List" \
+	--val_list "ImageNet Val List" \
+	--save './checkpoints/' \
+	--workers 16 \
+	--epochs 250 \
+	--warmup_epochs 5 \
+	--batch_size 512 \
+	--lr 0.2 \
+	--lr_min 0.0 \
+	--lr_scheduler 'cosine_epoch' \
+	--momentum 0.9 \
+	--weight_decay 3e-5 \
+	--no_wd_bias_bn \
+	--model 'MobileNetV3_Large' \
+	--num_classes 1000 \
+	--dropout_rate 0.2 \
+	--label_smooth 0.1 \
+	--trans_mode 'tv' \
+	--color_jitter \
+	--ema_decay 0.9999 \
+	--opt_level 'O1' \
+	--note 'try'
 
 
